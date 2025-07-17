0cbe3322305d23c655365bfabd22613c23e2f782
varuni24 <varunigupta24@gmail.com>
2025-07-17 13:39:35 -0400

    fixed sql errors
    - moved from 1.0 to 3.0
    - renamed as per prof's advice
    - fixed errors inside them so that they apply on each other

--- config/schema/3.0/patches/db_schema_patch-3.ai-transcription.sql
+++ config/schema/3.0/patches/db_schema_patch-3.ai-transcription.sql
@@ -4,19 +4,19 @@ INSERT IGNORE INTO schemaversion (versionnumber) values ("ai-transcription-patch
 -- Create ocr_results table
 DROP TABLE IF EXISTS `ocr_results`;
 CREATE TABLE `ocr_results` (
-  `imgid` int(10) unsigned NOT NULL,
+  `mediaID` int(10) unsigned NOT NULL,
   `batchID` int(11) NOT NULL,
   `collID` int(11) NOT NULL,
   `results` JSON NOT NULL,
   `processed_date` timestamp NOT NULL,
-  PRIMARY KEY (`imgid`,`batchID`),
-  KEY `FK_ocr_results_img` (`imgid`),
+  PRIMARY KEY (`mediaID`,`batchID`),
+  KEY `FK_ocr_results_media` (`mediaID`),
   KEY `FK_ocr_results_batch` (`batchID`),
-  CONSTRAINT `FK_ocr_results_img` FOREIGN KEY (`imgid`) REFERENCES `images` (`imgid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_ocr_results_media` FOREIGN KEY (`mediaID`) REFERENCES `media` (`mediaID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_ocr_results_batch` FOREIGN KEY (`batchID`) REFERENCES `batch` (`batchID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Add columns to the omoccurrences to indicate whether the record was human or machine generated and its confidence value (if machine generated)
-ALTER TABLE `omoccurrences` 
+ALTER TABLE `omoccurrences`
     ADD COLUMN `is_machine_generated` BOOL DEFAULT 0,
     ADD COLUMN `confidence_value` double DEFAULT NULL;
